a5d343d96c3fc656ff134e7c458373142e484d05
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-02-10 11:11:30 +0200

    end of day

--- app.js
+++ app.js
@@ -13,6 +13,9 @@
     console.log(this);
     //console.log('moosipurgi sees');
 
+    //KÕIK MUUTJUJAD, mis on üldised ja muudetavad
+    this.click_count = 0;
+
     this.init();
   };
 
@@ -28,7 +31,9 @@
       document.querySelector('.add-new-jar').addEventListener('click',this.addNewClick.bind(this));
     },
     addNewClick: function(event){
-      console.log(event);
+      //console.log(event);
+      this.click_count++;
+      console.log(this.click_count);
     }
 
   };
