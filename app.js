3524c9f5500e914be51c427da2948eb728ac4482
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-02-10 10:49:40 +0200

    singelton pattern

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-js
+(function(){
+  "use strict";
+
+
+  var Moosipurk = function(){
+
+    //SINGELTON PATTERN
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+    Moosipurk.instance = this; //this viitab moosipurgile
+
+    console.log('moosipurgi sees');
+  };
+
+  window.onload = function(){
+    var app = new Moosipurk();
+
+
+  };
+
+
+
+})();
